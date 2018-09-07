735f66ecded977b33b2955427aa25914327f7bf5
Jordan Wood <wjordanwood@gmail.com>
2018-09-06 21:13:23 -0500

    NavBarAndAboutPage (#1)
    * - Added Images and first content for about page
    - Added Tournament logic to navigation
    
    * - moved project location
    
    * - gitignore update
    
    * - remove vs folder for git
    
    * - Updated Navigation to Include new views
    - added new image assets
    - added new actions in tournament controller to correspond to views created in nav
    - added new views for the tournament controller and gave them a basic skeleton
    
    * NavBarAndAboutPage
    ### Content\styles
    - rearranged to start using individual scss files instead of just site.scss
    
    ### Views
    #### Home\About
    - added summary
    - added video
    - added link to graffiti's give page
    
    #### Tournament
    ##### About
    - added WHS address
    
    ##### Standings
    - added headers for each bracket section
    
    * NavBarAndAboutPage
    ### Content\styles\views\_home
    - added margin to the bottom of testimonial images
    
    ### Views\Home\About
    - added wrapper div for view and testimony section

---
+++ GraffitiSite/Controllers/TournamentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GraffitiSite.Controllers
+{
+    public class TournamentController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Registration");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Standings()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
