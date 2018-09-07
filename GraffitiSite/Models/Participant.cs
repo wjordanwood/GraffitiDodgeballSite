791510583ce18f905eb05810fcd276a964363bc1
Jordan Wood <JordanW@exigo.com>
2018-09-06 23:20:01 -0500

    PaypalServiceIntegration ### Controllers\RegistrationController - added registration controller to take care of all user registration
    ### Models\Participant
    - added a model for tournament participants
    
    ### Views
    #### Registration\Index
    - moved the file from "Tournament\Register" to this location
    
    #### Shared\_Layout
    - readdressed URL for registration page
    
    #### Tournament
    ##### About
    - readdressed URL for registration page
    
    ##### Register
    - removed this view
    
    #### packages.config
    - added reference to Paypal SDK
    
    #### Web.config
    - added Paypal References
    - added variables for Paypal App Keys

---
+++ GraffitiSite/Models/Participant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GraffitiSite.Models
+{
+    public class Participant
+    {
+        [Display(Name="First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Display(Name = "Division")]
+        public int DivisionID { get; set; }
+
+        [Display(Name = "Team Name")]
+        public string TeamName { get; set; }
+
+        [Display(Name = "Grade")]
+        public int Grade { get; set; }
+    }
+}
